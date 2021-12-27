03262d9df8ff2fd5dcfdee367a023e50a9d98fcc
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-27 14:29:28 +0500

    Getting city API call

---
+++ scripts/forecast.js
@@ -0,0 +1,16 @@
+const key = "XmwCUfYTISIiuL0xGg1A7hah46mTxpdR";
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("tashkent")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
