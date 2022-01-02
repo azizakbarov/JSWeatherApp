a0822175b268732540dff2760c8fd3b2a43385f5
Aziz Akbarov <azizbakbarov@gmail.com>
2022-01-02 11:44:39 +0500

    Get Weather API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = "XmwCUfYTISIiuL0xGg1A7hah46mTxpdR";
 
+// get weather information
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
 // get city information
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
@@ -12,5 +24,10 @@ const getCity = async (city) => {
 };
 
 getCity("tashkent")
-  .then((data) => console.log(data))
+  .then((data) => getWeather(data.Key))
+  .then((data) => {
+    console.log(data);
+  })
   .catch((err) => console.log(err));
+
+// getWeather(351199);
